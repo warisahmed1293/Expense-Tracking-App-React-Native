047e40b4e7ae53f8532811b5fb7d9d77b0e9a5ee
Waris Ahmed <warisahmed2022@gmail.com>
2024-09-12 23:27:45 +0500

    first commit

--- src/components/HomeScreen/BalanceCard.tsx
+++ src/components/HomeScreen/BalanceCard.tsx
@@ -6,41 +6,29 @@ import Icon from "../Icon";
 const BalanceCard = () => {
     return (
         <View className="bg-DARK_GREEN w-[335px] h-[225px] flex-1 flex-row justify-between items-start rounded-3xl ">
-            <View className="flex-1 my-5 justify-between px-2">
-                <View className="flex">
-                    <Text color="white" fontSize="22px">
-                        Total Balance
-                    </Text>
-                    <Text color="white" fontSize="32px" fontWeight="700">
-                        $ 2,548.00
-                    </Text>
+            <View className="flex flex-col justify-between py-5 px-2">
+                <View className="flex items-center">
+                    <Text className="text-white text-2xl">Total Balance</Text>
+                    <Text className="text-white text-4xl font-bold">$ 2,548.00</Text>
                 </View>
-                <View className="flex">
-                    <View className="flex flex-row items-center">
-                        <Icon name="ArrowDownIcon" size={24} type="solid" color="white" />
-                        <Text color="white" fontSize="18px" className="ml-2">
-                            Income
-                        </Text>
+                <View className="flex items-center">
+                    <View className="flex items-center">
+                        <Icon name="ArrowDownIcon" className="text-white w-6 h-6" type="solid" />
+                        <Text className="text-white text-base ml-2">Income</Text>
                     </View>
-                    <Text color="white" fontSize="26px" fontWeight="700">
-                        $ 1,840.00
-                    </Text>
+                    <Text className="text-white text-3xl font-bold">$ 1,840.00</Text>
                 </View>
             </View>
-            <View className="flex my-5 justify-between px-4">
-                <View className="flex">
-                    <Icon name="EllipsisHorizontalIcon" size={34} type="solid" color="white" />
+            <View className="flex flex-col justify-between py-5 px-4">
+                <View className="flex items-center">
+                    <Icon name="EllipsisHorizontalIcon" className="text-white w-8 h-8" type="solid" />
                 </View>
-                <View className="flex">
-                    <View className="flex flex-row items-center">
-                        <Icon name="ArrowUpIcon" size={24} type="solid" color="white" />
-                        <Text color="white" fontSize="18px" className="ml-2">
-                            Expenses
-                        </Text>
+                <View className="flex items-center">
+                    <View className="flex items-center">
+                        <Icon name="ArrowUpIcon" className="text-white w-6 h-6" type="solid" />
+                        <Text className="text-white text-base ml-2">Expenses</Text>
                     </View>
-                    <Text color="white" fontSize="28px" fontWeight="700">
-                        $ 284.00
-                    </Text>
+                    <Text className="text-white text-2xl font-bold">$ 284.00</Text>
                 </View>
             </View>
         </View>
